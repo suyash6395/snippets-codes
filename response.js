746d6e7a58f96c76f97584e6c7805ce746a2c543
Sheel <35765579+sushilr007@users.noreply.github.com>
2018-02-26 12:16:12 +0530

    response.js

---
+++ response.js
@@ -0,0 +1,11 @@
+// "z" is a object provided by Zapier. 
+**converting response string to array** 
+var ob =  JSON.parse(response.content); //accepting response as string and converting to object
+           z.console.log("is ob array",Array.isArray(ob)); // to check whether it is array
+           const bb = Array.prototype.slice.call(ob); // converting object  "ob" in array
+           z.console.log("is bb array",Array.isArray(bb)); 
+           z.console.log ("p1",response);
+           return bb; // returning array present in "bb" 
+           
+           
+           
